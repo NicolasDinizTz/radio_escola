765c521fee7515e34149b0d748e54d754c824d6d
NicolasDinizTz <147161700+NicolasDinizTz@users.noreply.github.com>
2023-12-01 10:17:49 -0300

    fghf

--- main.js
+++ main.js
@@ -15,16 +15,6 @@ const listaTeclas = document.querySelectorAll('.tecla');
 
 var contador = 0
 
-//while (contador< listaTeclas.length) {
-//    const botao = listaTeclas[contador];
-//    const instrumento = botao.classList[1];
-//    console.log(botao);
-//   console.log(instrumento);
-//    const idAudio = `#som_${instrumento}`;
-//    console.log(idAudio);
-//    botao.onclick = function () { tocaSom(idAudio);}
-//    contador++;
-//}
 
 for (let i = 0; i < listaTeclas.length; i++) {
     const tecla = listaTeclas[i];
